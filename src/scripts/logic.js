3eb85a30d94ad71c5f00f2f4c51e294b79647fea
Simon <sima@chetverukhin.ru>
2017-07-07 01:24:44 +0300

    new ui begins, we make sum

--- src/scripts/logic.js
+++ src/scripts/logic.js
@@ -2,37 +2,37 @@ function numberWithSpaces(x) {
 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 }
 
-function dealWithInput(targetInput) {
-	let current = $(targetInput).val();
-	let signs = current.length;
-
-	if (signs === 0) {
-		$('.rouble').addClass('rouble--grey');
-		return;
-	} else {
-		$('.rouble').removeClass('rouble--grey');
-	}
-
-	if (signs === 4) {
-		signs++;
-	}
-
-	let currentNumber = parseInt(current.replace(/\s+/g, ''));
-	let sendBack = numberWithSpaces(currentNumber);
-
-	console.log(currentNumber);
-
-	if (currentNumber >= 500) {
-		$('.button-exchange, .button-apply').show();
-	} else if (currentNumber >= 100) {
-		$('.button-apply').show();
-	} else {
-		$('.button-exchange, .button-apply').hide();
-	}
-
-	$(targetInput).css('width', signs * 23);
-	$(targetInput).val(sendBack);
-}
+// function dealWithInput(targetInput) {
+// 	let current = $(targetInput).val();
+// 	let signs = current.length;
+//     let sendBack = 0;
+//     let currentNumber;
+//
+//     console.log(current);
+//
+//     if (signs > 0) {
+//         $('.rouble').removeClass('rouble--grey');
+//     } else {
+//         $('.rouble').addClass('rouble--grey');
+//     }
+//
+//     if (current === '') {
+//         console.log('true');
+//         currentNumber = parseInt(current.replace(/\s+/g, ''));
+//     } else {
+//         currentNumber = parseInt(current.replace(/\s+/g, ''));
+//     	sendBack = numberWithSpaces(currentNumber);
+//     }
+//
+//     if (sendBack === 0) {
+//         sendBack = ''
+//     }
+//
+//     $('.input-span').text(sendBack);
+//     let spanWidth = $('.input-span').width() * 1.1;
+//     $(targetInput).width(spanWidth);
+// 	$(targetInput).val(sendBack);
+// }
 
 let userData = {
 	'currentSum': 0
@@ -61,15 +61,39 @@ var data = JSON.parse(localStorage.getItem('atm'));
 // Вбиваем данные
 updateData();
 
-$('.button-exchange').click(function (e) {
-	e.preventDefault();
-	let current = $('.input-sum').val();
-	let sum = parseInt(current.replace(/\s+/g, ''));
-	data.currentSum = sum;
-	updateStorage();
-	window.location.href = 'exchange.html';
-});
+var inputText = '';
 
-$('.input-sum').on('keydown keyup', function () {
-	dealWithInput(this);
-});
+function showNumber(x) {
+	x = parseInt(x.replace(/\s+/g, ''));
+	if (isNaN(x)) {
+		$('.input-cash').text('');
+		$('.rouble').addClass('rouble--grey');
+	} else if (x < 1) {
+		$('.input-cash').text('');
+		$('.rouble').addClass('rouble--grey');
+	} else {
+		x = numberWithSpaces(x);
+		$('.input-cash').text(x);
+	}
+}
+
+document.onkeyup = function (e) {
+	var key = e.key;
+	var isNotNumber = isNaN(parseInt(key));
+
+	if (!isNotNumber) {
+		if ((inputText + key) > 200000) {
+			$('.subtitle-sum').addClass('subtitle--error');
+		} else if ((inputText + key) < 1) {
+		} else {
+			inputText += key;
+			$('.rouble').removeClass('rouble--grey');
+			showNumber(inputText, true);
+		}
+	} else if (key === 'Backspace') {
+		inputText = inputText.slice(0, -1);
+		$('.subtitle-sum').removeClass('subtitle--error');
+		showNumber(inputText);
+	}
+
+};
